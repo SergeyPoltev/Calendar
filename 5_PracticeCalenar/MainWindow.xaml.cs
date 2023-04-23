03a21d3396fb096adb01ba2c7cadad9ee24a4df9
SergeyPoltev <isip.s.d.poltev@mpt.ru>
2023-04-23 23:49:35 +0300

    Календарь

---
+++ 5_PracticeCalenar/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5_PracticeCalenar
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Numbers first = new Numbers();
+            first.NumDays.Text= "1";
+            Numbers second = new Numbers();
+            second.NumDays.Text = "2";
+            FirstDay.Content = first;
+        }
+    }
+}
